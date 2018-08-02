51472189238ca1a0e5e5593f397233b244cedec6
kofkoy123 <125032854@qq.com>
2018-08-02 21:05:38 +0800

    Initial commit

---
+++ App/src/main/java/kshop/lzr/com/kotlin_shop/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package kshop.lzr.com.kotlin_shop.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kshop.lzr.com.kotlin_shop.R
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
